d02f3c5b979371a9219864904b6a1c715493a560
bum0w0 <kgy5988@naver.com>
2025-02-16 21:58:59 +0900

    #15 [Feat] 상품 및 상품 옵션 도메인과 레포지토리 추가

---
+++ src/main/java/com/kreamify/domain/product/domain/Product.java
@@ -0,0 +1,95 @@
+package com.kreamify.domain.product.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String englishName;
+
+    @Column(nullable = false)
+    private String koreanName;
+
+    @Column(nullable = false)
+    private String modelNumber;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String image;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private LocalDate releaseDate;
+
+    @Column(nullable = false)
+    private int releasePrice;
+
+    // TINYINT(0)으로 기본값 설정 (false)
+    @Column(nullable = false, columnDefinition = "TINYINT default 0")
+    private boolean isDeleted;
+
+    // 하나의 상품은 여러 개의 상품 옵션을 가질 수 있다.
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductOption> options = new ArrayList<>();
+
+    // JPA에서 Product 객체를 생성할 때 기본 생성자가 필요 (외부에서의 객체 생성을 막음)
+    protected Product() {
+
+    }
+
+    @Builder
+    private Product(
+            Long id,
+            String brand,
+            String englishName,
+            String koreanName,
+            String modelNumber,
+            String image,
+            String color,
+            LocalDate releaseDate,
+            int releasePrice
+    ) {
+        this.id = id;
+        this.brand = brand;
+        this.englishName = englishName;
+        this.koreanName = koreanName;
+        this.modelNumber = modelNumber;
+        this.image = image;
+        this.color = color;
+        this.releaseDate = releaseDate;
+        this.releasePrice = releasePrice;
+    }
+
+    // Product 객체에 상품 옵션을 추가
+    public void addOption(String option) {
+        this.options.add(buildProductOption(option));
+    }
+
+    // ProductOption의 빌더 객체를 생성
+    private ProductOption buildProductOption(String option) {
+        return ProductOption
+                .builder()
+                .product(this)
+                .size(option)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/kreamify/domain/product/domain/ProductOption.java
@@ -0,0 +1,44 @@
+package com.kreamify.domain.product.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "product_option")
+// 한 상품에 여러 개의 옵션(사이즈, 가격 등)을 부여 할 수 있다.
+public class ProductOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_option_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    // referencedColumnName 생략 가능 (Product 엔티티에서 id가 기본 키로 설정되어 있기 때문에 자동으로 기본 키 참조)
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private String size;
+
+    @Column(nullable = false, columnDefinition = "default 0")
+    private int lowestPrice;
+
+    @Column(nullable = false, columnDefinition = "default 0")
+    private int highestPrice;
+
+    protected ProductOption() {
+
+    }
+
+    // 객체를 생성할 때 빌더 패턴을 통해 각 필드를 설정할 수 있음.
+    // 내부적으로 빌더 클래스가 자동으로 생성됨 (모든 필드를 메서드 체이닝 방식으로 설정할 수 있는 메서드 제공)
+    @Builder
+    private ProductOption(Long id, Product product, String size) {
+        this.id = id;
+        this.product = product;
+        this.size = size;
+    }
+}
