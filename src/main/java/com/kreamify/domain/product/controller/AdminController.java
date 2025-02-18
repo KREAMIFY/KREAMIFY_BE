b81251886ee49f0db4fcd2b7b5062e1a4e8cd524
bum0w0 <kgy5988@naver.com>
2025-02-18 18:01:19 +0900

    #18 [Feat] 상품 수정 API 컨트롤러 추가

--- src/main/java/com/kreamify/domain/product/controller/AdminController.java
+++ src/main/java/com/kreamify/domain/product/controller/AdminController.java
@@ -6,31 +6,22 @@ import com.kreamify.global.response.ApiResponse;
 import com.kreamify.global.service.S3Uploader;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/admin/products")
+@RequiredArgsConstructor
 public class AdminController {
 
     private static final String DIRECTORY = "products";
 
     private final S3Uploader s3Uploader;
     private final ProductService productService;
-
-    public AdminController(
-            S3Uploader s3Uploader,
-            ProductService productService
-    ) {
-        this.s3Uploader = s3Uploader;
-        this.productService = productService;
-    }
 
     @Operation(summary = "상품 등록",
             description = "이미지와 함께 상품을 등록할 수 있습니다.")
@@ -43,6 +34,16 @@ public class AdminController {
         request.addImage(image);
 
         return ResponseEntity.ok(ApiResponse.of(productService.registerProduct(request)));
+    }
+
+    @Operation(summary = "상품 수정",
+            description = "상품의 정보를 수정할 수 있습니다.")
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<Long>> modifyProduct(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequest productRequest
+    ) {
+        return ResponseEntity.ok(ApiResponse.of(productService.modifyProduct(id, productRequest)));
     }
 
 }
