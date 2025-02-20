6db7345537063767872167e9a93549fd85b513ee
bum0w0 <kgy5988@naver.com>
2025-02-20 20:29:09 +0900

    #21 [Refactor] 상품 조회 메서드 분리

--- src/main/java/com/kreamify/domain/product/repository/ProductOptionRepository.java
+++ src/main/java/com/kreamify/domain/product/repository/ProductOptionRepository.java
@@ -4,7 +4,19 @@ import com.kreamify.domain.product.domain.Product;
 import com.kreamify.domain.product.domain.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
     // 해당 상품(Product)에 특정 사이즈(size) 옵션이 있는지 확인
     boolean existsByProductAndSize(Product product, String size);
+
+    Optional<ProductOption> findFirstByProductAndLowestPriceNotOrderByLowestPrice(
+            Product product,
+            int zero
+    );
+
+    Optional<ProductOption> findFirstByProductOrderByHighestPriceDesc(Product product);
+
+    Optional<ProductOption> findByProductAndSize(Product product, String size);
 }
--- src/main/java/com/kreamify/domain/product/repository/ProductRepository.java
+++ src/main/java/com/kreamify/domain/product/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.kreamify.domain.product.repository;
 import com.kreamify.domain.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // 삭제되지 않은 특정 id의 상품을 조회
-    Optional<Product> findByIdAndIsDeleted(Long id, boolean deleted);
+    List<Product> findAllByIsDeletedFalse();
+    Optional<Product> findByIdAndIsDeletedFalse(Long id);
 }
