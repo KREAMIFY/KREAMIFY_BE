a0f6d94fd4103153a4137a95266f5d6a8bef57a9
bum0w0 <kgy5988@naver.com>
2025-02-23 05:38:49 +0900

    #23 [Feat] 거래 도메인, DTO 설계

---
+++ src/main/java/com/kreamify/domain/deal/domain/SellingBid.java
@@ -0,0 +1,60 @@
+package com.kreamify.domain.deal.domain;
+
+import com.kreamify.domain.product.domain.Product;
+import com.kreamify.domain.user.domain.User;
+import com.kreamify.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "selling_bid")
+public class SellingBid extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private String size;
+
+    @Column(nullable = false)
+    private int suggestPrice;
+
+    @Column(nullable = false)
+    private int deadline;
+
+    @Column(columnDefinition = "VARCHAR(45) default '입찰 중'")
+    private String status;
+
+    protected SellingBid() {
+
+    }
+
+    @Builder
+    private SellingBid(
+            Long id,
+            User user,
+            Product product,
+            String size,
+            int suggestPrice,
+            int deadline
+    ) {
+        this.id = id;
+        this.user = user;
+        this.product = product;
+        this.size = size;
+        this.suggestPrice = suggestPrice;
+        this.deadline = deadline;
+    }
+
+}
