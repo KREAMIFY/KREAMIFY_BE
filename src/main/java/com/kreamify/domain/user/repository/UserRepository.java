0ddb9f038db5b4e1aea572ed0da134251891d499
sanchaehwa <rosie031123@naver.com>
2025-03-05 22:16:50 +0900

    #31 [Feat] 사용자 조회 기능 구현과정에서 Repo 오타 수정

--- src/main/java/com/kreamify/domain/user/repository/UserRepository.java
+++ src/main/java/com/kreamify/domain/user/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByIdAndIsDeletedFalse(Long id); //쿼리 메서드
-    boolean existsUserByEmail(String email); //툭종 아매알울 거잔 서용자가 DB에 존재하는지 여부를 확인
+    boolean existsUserByEmail(String email); //특정 이메일을 가진 사용자가 DB에 존재하는지 여부를 확인
 }
 /*
 SELECT * FROM user WHERE id = ? is_deleted = FALSE; // is_deleted : 삭제된 사용자는 조회되지 않도록 필터링
