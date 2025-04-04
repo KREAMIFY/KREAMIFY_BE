c670de96102203e89a8969d3560b13546c5539b7
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-04-05 00:23:30 +0900

    #71 [test] 구매 통합 테스트

---
+++ src/test/java/com/kreamify/domain/user/controller/UserBuyInfoControllerTest.java
@@ -0,0 +1,194 @@
+package com.kreamify.domain.user.controller;
+
+import com.kreamify.domain.config.TestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("local")
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestConfig
+@Transactional
+class UserBuyInfoControllerTest {
+    private static final Long USER_ID = 1L;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("구매 입찰 내역 조회 - 전체 테스트")
+    @Test
+    void getTotalBiddingHistoryTest() throws Exception {
+
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/bidding", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                );
+
+
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.bidHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.bidHistory.length()")
+                                .isNumber()
+                ); //2
+    }
+
+    @DisplayName("구매 입찰 내역 조회 - 특정 상태(조건) 테스트")
+    @Test
+    void getBiddingHistoryByStatusTest() throws Exception {
+        // given
+        String status = "기한만료";
+
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/bidding", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .param("status", status)
+                );
+
+        // then
+        int expectedLength = 2;
+
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.bidHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.bidHistory.length()")
+                                .isNumber()
+                );
+    }
+
+    @DisplayName("구매 거래 진행 중 내역 조회 - 전체 테스트")
+    @Test
+    void getTotalPendingHistoryTest() throws Exception {
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/pending", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                );
+
+
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory.length()")
+                                .isNumber()
+                );
+    }
+
+
+    @DisplayName("진행 중 내역 조회 - 특정 상태(조건) 테스트")
+    @Test
+    void getPendingHistoryByStatusTest() throws Exception {
+        // given
+        String status = "검수합격";
+
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/pending", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .param("status", status)
+                );
+
+        // then
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory.length()")
+                                .isNumber()
+                );
+    }
+
+    @DisplayName("구매 종료 내역 조회 - 전체 테스트")
+    @Test
+    void getTotalFinishedHistoryTest() throws Exception {
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/finished", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                );
+
+        // then
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory.length()")
+                                .isNumber()
+                );
+    }
+
+    @DisplayName("구매 종료 내역 조회 - 특정 상태(조건) 테스트")
+    @Test
+    void getFinishedHistoryByStatusTest() throws Exception {
+        // given
+        String status = "배송완료";
+
+        // when
+        ResultActions result = mockMvc
+                .perform(
+                        get("/users/{userId}/buying/finished", USER_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .param("status", status)
+                );
+
+        // then
+        result
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory")
+                                .isArray()
+                )
+                .andExpect(
+                        jsonPath("$.data.dealHistory.length()")
+                                .isNumber()
+                );
+    }
+}
